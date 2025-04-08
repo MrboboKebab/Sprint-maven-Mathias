8d60116f688174c18ec9702208af6a33fa54a8c4
MrboboKebab <mathias.bousquet@efrei.net>
2025-04-08 20:29:07 +0200

    first commit

---
+++ demo-mathias/demo-mathias/src/main/java/com/mathias/demo_mathias/DemoMathiasApplication.java
@@ -0,0 +1,13 @@
+package com.mathias.demo_mathias;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class DemoMathiasApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(DemoMathiasApplication.class, args);
+	}
+
+}
---
+++ demo-mathias/demo-mathias/src/test/java/com/mathias/demo_mathias/DemoMathiasApplicationTests.java
@@ -0,0 +1,13 @@
+package com.mathias.demo_mathias;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class DemoMathiasApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
