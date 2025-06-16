4d049f87b5ee1ba21b1feb283f6f04115c01f5b5
MrboboKebab <mathias.bousquet@efrei.net>
2025-06-16 12:34:30 +0200

    Update DemoMathiasApplicationTests.java

--- demo-mathias/demo-mathias/src/test/java/com/mathias/demo_mathias/DemoMathiasApplicationTests.java
+++ demo-mathias/demo-mathias/src/test/java/com/mathias/demo_mathias/DemoMathiasApplicationTests.java
@@ -3,11 +3,14 @@ package com.mathias.demo_mathias;
 import org.junit.jupiter.api.Test;
 import org.springframework.boot.test.context.SpringBootTest;
 
+import static org.junit.jupiter.api.Assertions.assertTrue;
+
 @SpringBootTest
 class DemoMathiasApplicationTests {
 
-	@Test
-	void contextLoads() {
-	}
+    @Test
+    void contextLoads() {
+        assertTrue(true);  // Une assertion simple pour valider que le test passe
+    }
 
 }
